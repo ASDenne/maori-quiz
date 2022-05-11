e5523d898980e6ed0a400d14cd0661d78568fc56
ASDenne <98923598+ASDenne@users.noreply.github.com>
2022-05-11 13:40:16 +1200

    G

--- Code testing.py
+++ Code testing.py
@@ -1,6 +1,12 @@
-import random
-numbers = ["Tahi","rua","toru","whā","rima","ono","whitu","waru","iwa"]
-Answer = random.randint(1,9)
-question = numbers[Answer]
-print(question)
-
+while True:
+    question = ""
+    numbers = ["Tahi","rua","toru","whā","rima","ono","whitu","waru","iwa"]
+    Answer = int(input(">>"))
+    if len(str(Answer)) == 1:
+        question = numbers[Answer-1].upper()
+    elif Answer == 10:
+        question = "Tekau"
+    elif Answer % 10 == 0:
+        question = numbers[(Answer//10)-1].upper() + " tekau"
+    print(question)
+    else:
