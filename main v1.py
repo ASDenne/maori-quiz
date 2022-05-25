03d9d8df678cd6bc1de8a3efe3e64eaa9db291a8
ASDenne <98923598+ASDenne@users.noreply.github.com>
2022-05-25 13:37:13 +1200

    N

--- main v1.py
+++ main v1.py
@@ -3,7 +3,7 @@ import random
 
 def instructions():
     print("the aim of the game is to guess what the maori number is in english")
-    print("thier is an easy mode and a hard mode")
+    print("their is an easy mode and a hard mode")
     print("the easy mode is numbers 1 through 9")
     print("the hard mode is numbers 1 through 99")
     print("when the  game ends you while be told how many you got right ")
@@ -14,68 +14,65 @@ def instructions():
 def yes_no(question_text):
     while True:
         print(question_text)
-        Answer = input(">>").lower()
-        # if they say output program countiues
-        if Answer == "yes" or Answer == "y" or Answer == "":
+        guess_ = input(">>").lower()
+        # if they say output program continues
+        if guess_ == "yes" or guess_ == "y" or guess_ == "":
             return "Yes"
 
-        #if they say no output desplay instructions
-        elif Answer == "no" or Answer == "n":
+        # if they say no output display instructions
+        elif guess_ == "no" or guess_ == "n":
             return "No"
 
-
-
-        #otherwise show error
+        # otherwise, show error
         else:
             print('please answer "yes" or "no"')
-        print(f"you entered {Answer}")
+        print(f"you entered {guess_}")
 
 
 def easy_hard(question_text):
     while True:
         print(question_text)
-        Answer = input(">>").lower()
+        answer_ = input(">>").lower()
         # if they say easy out put that
-        if Answer == "easy" or Answer == "e":
-            Answer = "Easy"
-            return Answer
+        if answer_ == "easy" or answer_ == "e":
+            answer_ = "Easy"
+            return answer_
 
-        #if they say hard output that
-        elif Answer == "hard" or Answer == "h":
-            Answer = "Hard"
-            return Answer
+        # if they say hard output that
+        elif answer_ == "hard" or answer_ == "h":
+            answer_ = "Hard"
+            return answer_
 
-        #otherwie show error
+        # otherwise, show error
         else:
             print('please answer "easy" or "hard"')
-        print(f"you entered {Answer}")
+        print(f"you entered {answer_}")
 
 
-
-#mainroutine
+# main routine
 rounds = 0
 mistakes = 0
-words= []
-instruction = yes_no("have you played before? :")
+words = []
+instruction = yes_no("have you played before? :")  # finding if they have played
 if instruction == "No":
-    instructions()
-diffuclty = easy_hard("do you want to play easy or hard mode :")
+    instructions()  # prints instructions
+difficulty = easy_hard("do you want to play easy or hard mode :")  # finding out what difficulty
 while True:
     Numbers = ["Tahi", "rua", "toru", "whā", "rima", "ono", "whitu", "waru", "iwa"]
 
-    if diffuclty == "Easy":
+    if difficulty == "Easy":
         answer: int = random.randint(1, 9)
     else:
         answer: int = random.randint(1, 99)
     if answer < 10:
-        question = Numbers[answer - 1]
-    elif answer == 10:
+        question = Numbers[answer - 1]  # 1-9
+    elif answer == 10:  # 10
         question = "Tekau"
-    elif answer < 20:
+    elif answer < 20:  # 11-19
         question = "tekau mā " + Numbers[answer % 10 - 1]
-    elif answer % 10 == 0:
+    elif answer % 10 == 0:  # ends in 0
         question = Numbers[answer // 10 - 1] + " tekau"
-    else:
+    else:  # everything else
         question = Numbers[answer // 10 - 1] + " tekau mā " + Numbers[answer % 10 - 1]
     print(f"what is {question} In english")
     while True:
@@ -91,10 +88,10 @@ while True:
         mistakes = mistakes + 1
         words.append(question)
     rounds = rounds + 1
-    stop = yes_no("do you want to countinue?")
+    stop = yes_no("do you want to continue?")
     if stop == "No":
         break
-print(F"you got {rounds-mistakes} questions right")
+print(F"you got {rounds - mistakes} questions right")
 print(F"you got {mistakes} questions wrong")
-print(F"your score is {rounds - mistakes} out of {rounds} or {((rounds-mistakes)/rounds)*100}%")
+print(F"your score is {rounds - mistakes} out of {rounds} or {((rounds - mistakes) / rounds) * 100}%")
 print(F"you need to work on {words}")
